1792eafcd63270a31f02f3a069e933245d841006
Duncan Bellamy <dunk@denkimushi.com>
2020-06-20 20:16:09 +0100

    add read git hash from file

--- version.py
+++ version.py
@@ -5,6 +5,10 @@ from json import loads
 from docker import from_env
 from natsort import natsorted
 
+def catFile( IMG, FILE ):
+	result = client.containers.run(IMG, "cat " + FILE).decode('utf-8').strip()
+	return result
+
 def getDockerTag( URI):
 	DOCKURI = "https://registry.hub.docker.com/v1/repositories/"
 	tags = request.urlopen(DOCKURI + URI + "/tags")
@@ -23,7 +27,6 @@ def getDockerTag( URI):
 	return lines[-1]
 
 def getAlpineApk( APK ):
-	client = from_env()
 	CMD = 'sh -c "apk update > /dev/null; apk info -s ' + APK + ';"'
 	IMG = "alpine"
 	if args.edge:
@@ -32,18 +35,20 @@ def getAlpineApk( APK ):
 	return result
 
 def getAlpineVer():
-	client = from_env()
-	CMD = 'cat /etc/alpine-release'
 	IMG = "alpine"
 	if args.edge:
 		IMG = IMG + ":edge"
-	result = client.containers.run(IMG, CMD).decode('utf-8').strip()
+	result = catFile(IMG, "/etc/alpine-release")
 	return result
 
 def getGitHash( FILE ):
 	data = request.urlopen("https://raw.githubusercontent.com/" + FILE)
 	etag = data.getheader('ETag')
 	return etag
+
+def getFileHash( IMG ):
+	result = catFile(IMG, "/etc/githash")
+	return result
 
 parser = ArgumentParser()
 parser.add_argument('-a', '--alpine', type=str,\
@@ -54,10 +59,13 @@ parser.add_argument('-e', '--edge', action='store_true',\
 help='use alpine edge version for command')
 parser.add_argument('-d', '--docker', type=str,\
 help='get latest docker tag of docker image "DOCKER"')
+parser.add_argument('-f', '--fhash', type=str,\
+help='get git hash saved in docker image "FHASH"')
 parser.add_argument('-g', '--ghash', type=str,\
 help='get latest git hash of file "GHASH"')
 
 args = parser.parse_args()
+client = from_env()
 
 if args.alpine:
 	print(getAlpineApk(args.alpine))
@@ -68,5 +76,9 @@ if args.docker:
 if args.base:
 	print(getAlpineVer())
 
+if args.fhash:
+	print(getFileHash(args.fhash))
+
 if args.ghash:
 	print(getGitHash(args.ghash))
+
