b7dce0c26ebb1c3089604a75a48fd7a7460f0571
Duncan Bellamy <dunk@denkimushi.com>
2020-07-13 22:04:56 +0100

    add passing build args for docker image labeling

--- docker-build.sh
+++ docker-build.sh
@@ -2,15 +2,30 @@
 
 echo "Building docker image: $IMAGE_NAME"
 echo "With tag: $1"
+echo "Alpine version in image: $2"
+echo "Main package Version: $3"
+ALP_VER="$2"
+VER="$3"
 
-#docker build --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` .
-#BUILD_DATE
-#ALP_VER
-#PKG_VER
+INFO=README.md
+  if [ -f "$INFO" ]; then
+    echo "Reading $INFO..."
+    NAME=$(head -1 README.md) | cut -d' ' -f2
+    DESC=$(head -2 README.md | tail -n1)
+  else
+    echo "$INFO not found"
+  fi
+
 URL=$(git config --get remote.origin.url)
 #GIT_COMMIT
 
-docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 -t "$IMAGE_NAME:$1" -t "$IMAGE_NAME" --push .
-RET="$?"
-docker logout
-exit "$RET"
+docker BUILDX build \
+--build-arg ALP_VER="$ALP_VER" \
+--build-arg VCS_URL="https://github.com/$TRAVIS_REPO_SLUG" \
+--build-arg BUILD_DATE=$(date -u +"%Y-%m-%d %H:%M:%S") \
+--build-arg NAME="$NAME" \
+--build-arg DESC="$DESC" \
+--build-arg VER="$VER" \
+--platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 \
+-t "$IMAGE_NAME:$1" -t "$IMAGE_NAME" --push .
+
