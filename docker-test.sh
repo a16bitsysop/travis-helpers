d33d874df7989062c43318941aae1877d2ef98c3
Duncan Bellamy <dunk@denkimushi.com>
2020-07-18 13:19:29 +0100

    remove ./ from scan.sh

--- docker-test.sh
+++ docker-test.sh
@@ -5,4 +5,4 @@ echo "Building docker test image for: $IMAGE_NAME"
 docker buildx build -t "test_image" --load .
 
 #set MICROSCANNER_TOKEN environment variable
-./scan.sh test_image
+scan.sh test_image
