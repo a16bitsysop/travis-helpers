c894a937ff8716229e0de4d81ad60e3c77f51efe
Duncan Bellamy <dunk@denkimushi.com>
2020-09-26 07:30:08 +0100

    tidy script add more status echo's

--- build-apk-native.sh
+++ build-apk-native.sh
@@ -11,6 +11,7 @@ else
 fi
 
 echo "Setting up build environment..."
+apk update
 apk add --no-cache --virtual .aport-deps git wget alpine-sdk pax-utils atools git sudo
 adduser -D ${NME} && addgroup ${NME} abuild && addgroup ${NME} tty
 
@@ -23,13 +24,14 @@ grep 'ls-blob' | sed "s+blame+plain+" | sed -r "s+.*ls-blob.*href='(.*)'.*+\1+" 
 echo "Extracted filenames: $afiles"
 
 mkdir -p aport
-cd aport && rm *
+cd aport
 for afile in $afiles
 do
   echo "Downloading $afile"
   wget -q "https://git.alpinelinux.org$afile"
 done
 
+echo "Preparing to build $tobuild"
 [ -f ../APKBUILD.patch ] && patch -p1 -i ../APKBUILD.patch
 [ -f ../prebuild.sh ] && sh ../prebuild.sh
 cp ../newfiles/* .
@@ -37,7 +39,8 @@ cp ../newfiles/* .
 cd ..
 mv aport /home/"$NME"/
 chown -R "$NME":"$NME" /home/"$NME"/aport
-su -c 'echo "Running as $(whoami)"  && cd && cd aport && abuild-keygen -a -i -n && abuild checksum && abuild -r' - ${NME}
+echo "Building $tobuild"
+su -c 'echo "Running as $(whoami)"  && cd ~/aport && abuild-keygen -a -i -n && abuild checksum && abuild -r' - ${NME}
 
 echo "Copying Packages"
 cp -a /home/"$NME"/packages .
