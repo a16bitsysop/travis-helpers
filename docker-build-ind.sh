fae4efd90f8a430c506c02c3c7debdd5f64b2034
Duncan Bellamy <dunk@denkimushi.com>
2020-10-04 16:42:56 +0100

    add script to build platforms individualy first so cached

---
+++ docker-build-ind.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Building docker test image for: $IMAGE_NAME"
+
+PLATFORMS="linux/386 linux/ppc64le linux/s390x linux/arm64 linux/arm/v7"
+
+for PLAT in $PLATFORMS
+do
+  docker buildx build -t "test_image" --platform $PLAT --load .
+done
