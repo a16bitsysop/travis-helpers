d5162418dd2aba40ce867ec0cd4c1f974511df99
Duncan Bellamy <dunk@denkimushi.com>
2020-10-17 09:21:01 +0100

    tidy script

--- set-timezone.sh
+++ set-timezone.sh
@@ -10,17 +10,17 @@ end='\e[0m'
 
 printf "%b" "${grn}"
 apk update
-echo "\$TIMEZONE= $TIMEZONE"
+echo " \$TIMEZONE= $TIMEZONE"
 echo
 
 if [ -n "$TIMEZONE" ]
 then
-  echo "Waiting for DNS"
+  echo " Waiting for DNS"
   ping -c1 -W60 google.com || ping -c1 -W60 www.google.com || ping -c1 -W60 google-public-dns-b.google.com
   apk add --no-cache tzdata
   if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]
   then
-    echo "Setting timezone to $TIMEZONE"
+    echo " Setting timezone to $TIMEZONE"
     cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
     echo "$TIMEZONE" > /etc/timezone
   else
@@ -28,5 +28,4 @@ then
   fi
   apk del tzdata
 fi
-[ -n "$1" ] && NAME="$1" || NAME="container"
-printf "%b\n" "${yel} Starting $NAME at $(date +'%x %X') ${end}"
+printf "%b\n" "${yel} Starting ${1-container} at $(date +'%x %X') ${end}"
